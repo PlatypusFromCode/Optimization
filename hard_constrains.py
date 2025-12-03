b53c170d64412e34292978cd0b982ad22cbaabe9
PlatypusFromCode <apolinary741@gmail.com>
2025-12-03 10:06:58 +0100

    additional hard constrains + json loaders

---
+++ hard_constrains.py
@@ -0,0 +1,106 @@
+from gurobipy import Model, GRB
+from structures import Teacher, Course, Room
+import gurobipy as gp
+#import structures_loaders
+
+
+
+
+
+
+
+def add_single_room_single_course_constr(m, x, slots, teachers, courses, rooms):
+    for r in rooms:                                                          # for every room r in R and for every time slot s in S
+        for s in slots:                                                      # there is more than 1 teacher t teaching course c
+            m.addConstr(
+                gp.quicksum(x[s, t, c, r] for t in teachers for c in courses) <= 1
+            )
+
+
+def add_single_teacher_single_course_constr(m, x, slots, teachers, courses, rooms):
+
+    for t_id in teachers:
+        for slot in slots:
+            m.addConstr(
+                gp.quicksum(
+                    x[slot, t_id, c_id, r_id]
+                    for c_id in courses
+                    for r_id in rooms
+                ) <= 1
+            )
+
+
+
+
+
+
+
+def add_no_semester_overlapping_constr(m, x, semesters, course_by_id,  slots, teachers, courses, rooms):
+
+    for sem in semesters:
+
+
+        courses_in_sem = [
+            c_id for c_id in courses
+            if course_by_id[c_id].semester == sem
+        ]
+
+        for slot in slots:
+            m.addConstr(
+                gp.quicksum(
+                    x[slot, t_id, c_id, r_id]
+                    for c_id in courses_in_sem
+                    for t_id in teachers
+                    for r_id in rooms
+                ) <= 1,
+            )
+
+
+
+def add_teacher_hard_time_constraints(m, x, teachers, teacher_by_id, courses, rooms):
+
+    for t_id in teachers:
+        teacher = teacher_by_id[t_id]
+
+        for slot in teacher.hard_time_constr:
+            m.addConstr(
+                gp.quicksum(
+                    x[slot, t_id, c_id, r_id]
+                    for c_id in courses
+                    for r_id in rooms
+                ) == 0
+            )
+
+def add_room_capacity_constr(
+    m, x, courses, rooms, course_by_id, room_by_id, slots, teachers
+):
+
+    for c_id in courses:
+        course = course_by_id[c_id]
+
+        for r_id in rooms:
+            room = room_by_id[r_id]
+
+            if course.expected_num_students > room.capacity:
+                for slot in slots:
+                    for t_id in teachers:
+                        m.addConstr(
+                            x[slot, t_id, c_id, r_id] == 0
+                        )
+
+def add_room_type_constraints(
+    m, x, slots, teachers, courses, rooms, course_by_id, room_by_id
+):
+
+    for slot in slots:
+        for t_id in teachers:
+            for c_id in courses:
+                course = course_by_id[c_id]
+                allowed_types = set(course.facility_constr)
+
+                for r_id in rooms:
+                    if room_by_id[r_id].type not in allowed_types:
+                        m.addConstr(
+                            x[slot, t_id, c_id, r_id] == 0
+                        )
+
---
+++ model.py
@@ -0,0 +1,35 @@
+from gurobipy import Model, GRB
+import hard_constrains
+from structures import Teacher, Course, Room
+import gurobipy as gp
+import hard_constrains
+#import structures_loaders
+
+m = Model()
+
+teachers: list[Teacher] = [] #probably use loaders here
+courses: list[Course] = []
+rooms: list[Room] = []
+
+T = [t.teacher_id for t in teachers] #lists of ids
+C = [c.course_id for c in courses]
+R = [r.room_id for r in rooms]
+
+slots = range(31)
+
+teacher_by_id = {t.teacher_id: t for t in teachers} #dict in form of obj_id:obj
+course_by_id = {c.course_id: c for c in courses} #dict in form of obj_id:obj
+room_by_id = {r.room_id: r for r in rooms}
+
+semesters = set(c.semester for c in courses) # for example SC4DMsem1
+
+x = m.addVars(slots, T, C, R, vtype=GRB.BINARY, name="x") #x[slot, teacher, course, room]
+
+hard_constrains.add_single_room_single_course_constr(m,x,slots, teachers, courses, rooms)
+hard_constrains.add_single_teacher_single_course_constr(m,x, slots, teachers, courses, rooms)
+hard_constrains.add_no_semester_overlapping_constr(m,x,semesters, course_by_id,slots, teachers, courses, rooms)
+hard_constrains.add_teacher_hard_time_constraints(m, x, teachers, teacher_by_id, courses, rooms)
+hard_constrains.add_room_capacity_constr(m, x, courses, rooms, course_by_id, room_by_id, slots, teachers)
+hard_constrains.add_room_type_constraints(m, x, slots, teachers, courses, rooms, course_by_id, room_by_id)
+
+
